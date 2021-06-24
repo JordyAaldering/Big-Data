b626be12db544dbd7bedc7f6e89167e7a2d1035f
Jordy Aaldering <aalderingjordy@gmail.com>
2021-06-24 13:57:24 +0200

    Remove shell code

--- Assignment06/submitCounter.sh
+++
@@ -1,7 +0,0 @@
-spark-submit \
-    --deploy-mode cluster \
-    --num-executors 20 \
-    --queue gold \
-    target/scala-2.12/LinkedDomainsCounter-assembly-1.0.jar \
-    /single-warc-segment \
-    /user/JordyAaldering/out
--- Assignment06/submitReducer.sh
+++
@@ -1,8 +0,0 @@
-spark-submit \
-    --deploy-mode cluster \
-    --num-executors 20 \
-    --queue gold \
-    target/scala-2.12/ReduceDomains-assembly-1.0.jar \
-    /user/JordyAaldering/out \
-    /user/JordyAaldering/reduced \
-    100
